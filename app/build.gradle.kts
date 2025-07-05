480c0ebbc8b775573e7098129700ce11c6cda066
DESKTOP-2F6PBUI\tbinh <tbinhduong0101@gmail.com>
2025-07-05 14:31:34 +0700

    Merge branch 'Dao' into Duong
    # Conflicts:
    #	.idea/misc.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:3.0.0")
     implementation("com.squareup.retrofit2:converter-gson:3.0.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    implementation("com.google.zxing:core:3.5.1")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 }
